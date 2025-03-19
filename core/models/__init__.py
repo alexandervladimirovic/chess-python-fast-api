831aad539e28621bb6179e26e624219cd84193d5
Alexander <sasha.beloglazoov@gmail.com>
2025-03-19 09:29:03 +0300

    fix error: __all__ contain unnecessary model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ("Base", "DatabaseHelper", "User", "db_helper")
+__all__ = ("Base", "DatabaseHelper", "db_helper")
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
