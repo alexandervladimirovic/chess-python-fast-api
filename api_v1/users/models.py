05beef78e3ca10b6012c4aad6ff3174986b3f563
Alexander <sasha.beloglazoov@gmail.com>
2025-03-21 00:00:41 +0300

    update profile models and create country and rank models.

--- api_v1/users/models.py
+++ api_v1/users/models.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 from typing import Optional
 
 from sqlalchemy import ForeignKey, String, func
+from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
@@ -10,16 +11,18 @@ from core.models import Base
 class User(Base):
     """Model for users.
 
+    Represents user in system with basic account information.
+
     Attributes:
         username (str): Unique username for user.
         email (str): Unique e-mail for user.
-        password_hash (str): Hashed password user.
-        date_joined (datetime): Datetime of user registration.
-        last_login (datetime): Datetime of user last login.
-        is_active (bool): Activity status user.
+        password_hash (str): Hashed password of user for authentication.
+        date_joined (datetime): Date and time when the user registered.
+        last_login (datetime): Last date and time when the user logged in.
+        is_active (bool): Flag whether user account is active. Default is True.
 
-    Link:
-        profile (Profile): Link to user profile (One-To-One).
+    Relationships:
+        profile (Profile): One-to-one relationship with the user profile.
 
     """
 
@@ -40,21 +43,31 @@ class User(Base):
     # link to 'Profile' model
     profile: Mapped["Profile"] = relationship(back_populates="user")
 
+    def __repr__(self):
+        return f"<User({self.id=}, {self.username=}, {self.email=})>"
+
 
 class Profile(Base):
-    """Model profile user. Contains the user personal info.
+    """Model profile user.
+
+    Contains detailed personal information for user.
 
     Attributes:
         name (str): First name for user (Optional).
         surname (str): Last name for user (Optional).
-        date_of_birth: Date of birth for user (Optional).
+        gender (GenderEnum): Gender of user. It uses `GenderEnum` for possible values.
+        date_of_birth: Birth date of the user (Optional).
         biography (str): Short biography for user (Optional).
         avatar_url (str): URL avatar for user (Optional).
         updated_at (datetime): Datetime last update profile.
         user_id (int): ID of user associated with profile (One-To-One).
+        country_id (int): ID of user country, linked to `Country` model.
+        rank_id (int): ID of user rank, linked to `Rank` model. Default is None.
 
-    Link:
-        user (User): Link to user profile (One-To-One).
+    Relationships:
+        user (User): One-To-One relationship with `User` model.
+        country (Country): One-To-Many relationship with the `Country` model.
+        rank (Rank): One-To-Many relationship with the `Rank` model.
 
     """
 
@@ -62,6 +75,9 @@ class Profile(Base):
 
     name: Mapped[Optional[str]] = mapped_column(String(25))
     surname: Mapped[Optional[str]] = mapped_column(String(35))
+    gender: Mapped[ENUM] = mapped_column(
+        ENUM("Male", "Female", "Not Defined", name="gender_enum"), default="Not Defined"
+    )
     date_of_birth: Mapped[Optional[date]] = mapped_column()
     biography: Mapped[Optional[str]] = mapped_column(String(300))
     avatar_url: Mapped[Optional[str]] = mapped_column(String(255))
@@ -69,7 +85,76 @@ class Profile(Base):
         default=func.now(),
         onupdate=func.now(),
     )
+    # One-To-One
     user_id: Mapped[int] = mapped_column(ForeignKey(User.id), unique=True)
 
-    # link to 'User' model
+    # One-To-Many
+    country_id: Mapped[int] = mapped_column(ForeignKey("countries.id"))
+    rank_id: Mapped[Optional[int]] = mapped_column(ForeignKey("ranks.id"), default=None)
+
+    # link to models
     user: Mapped["User"] = relationship(back_populates="profile")
+    country: Mapped["Country"] = relationship(back_populates="profiles")
+    rank: Mapped[Optional["Rank"]] = relationship(back_populates="profiles")
+
+    def __repr__(self):
+        return f"<Profile({self.user_id=}, {self.name if self.name else 'No name'})>"
+
+
+class Country(Base):
+    """Model country.
+
+    Represents a country where user is located or associated with.
+
+    Attributes:
+        name (str): The name of the country. This is unique.
+        code (str): The ISO 2-letter code of the country. This is unique.
+        description (str): Optional description for country.
+
+    Relationships:
+        profiles (Profile): Many-To-One relationship with the `Profile` model,
+        representing all users from this country.
+
+    """
+
+    __tablename__ = "countries"
+
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+    code: Mapped[str] = mapped_column(String(2), unique=True)
+    description: Mapped[Optional[str]] = mapped_column(String(300))
+
+    # Link to 'Profile' model
+    profiles: Mapped[Profile] = relationship(back_populates="country")
+
+    def __repr__(self):
+        return f"<Country({self.name} ({self.code}))>"
+
+
+class Rank(Base):
+    """Model rank.
+
+    Represents title associated with user.
+
+    Attributes:
+        name (str): The name of rank (e.g., Grandmaster, International Master).
+        acronym (str): The abbreviation for rank (e.g., GM, IM).
+        description (str): Optional description for rank.
+
+    Relationships:
+        profiles (Profile): Many-To-One relationship with `Profile` model,
+        representing all users with this rank.
+
+
+    """
+
+    __tablename__ = "ranks"
+
+    name: Mapped[str] = mapped_column(String(30), unique=True)
+    acronym: Mapped[str] = mapped_column(String(3), unique=True)
+    description: Mapped[Optional[str]] = mapped_column(String(300))
+
+    # Link to 'Profile' model
+    profiles: Mapped[Profile] = relationship(back_populates="rank")
+
+    def __repr__(self):
+        return f"<Rank({self.name} ({self.acronym}))>"
