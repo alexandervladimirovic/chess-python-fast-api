f3202425ad9ce69972dd1be32290ebbd03606f50
Alexander <sasha.beloglazoov@gmail.com>
2025-03-23 03:18:27 +0300

    create timestamp mixin

--- api_v1/users/mixin.py
+++ api_v1/users/mixin.py
@@ -1,14 +1,29 @@
+from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import String
+from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class DescriptionMixin(object):
-    """Mixin for adding description field."""
+    """Mixin for adding description field in tables."""
 
     _max_len_description_field = 300
 
     description: Mapped[Optional[str]] = mapped_column(
         String(_max_len_description_field)
+    )
+
+
+class TimestampMixin(object):
+    """Mixin for adding created_at and updated_at fields in tables."""
+
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.now,
+        server_default=func.now(),
+        onupdate=datetime.now,
+        server_onupdate=func.now(),
     )
