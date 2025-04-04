6b9a37b8acaa8c6340e985a639867a03665b0287
Alexander <sasha.beloglazoov@gmail.com>
2025-04-04 12:57:36 +0300

    add logging

--- api_v1/auth/utils.py
+++ api_v1/auth/utils.py
@@ -1,84 +1,23 @@
-import datetime
+from argon2.exceptions import HashingError, InvalidHashError, VerifyMismatchError
 
-import jwt
+from core.config import ph
+from logger import setup_logging
 
-from core.config import ph, settings
+from .exceptions import PasswordHashError, PasswordHashingIsError
 
-
-def encode_jwt(
-    payload: dict,
-    private_key: str = settings.jwt.private_key_path.read_text(),
-    algorithm: str = settings.jwt.algorithm,
-    expire_minutes: int = settings.jwt.access_token_expires_in_minutes,
-    expire_timedelta: datetime.timedelta | None = None,
-):
-    """Encode data (payload) in JWT using private key and algorithm.
-
-    Parameters
-    ----------
-        payload (dict): Data that will be encoded in JWT.
-        private_key (str): Private key for signing the JWT.
-        algorithm (str): Algorithm for signing JWT.
-        expire_minutes (int): Token lifetime in minutes.
-        expire_timedelta (timedelta | None): Optional timedelta for setup lifespan
-        token.
-
-    Returns
-    -------
-        str: Encoded JWT as string.
-
-    """
-    to_encode = payload.copy()
-    now = datetime.datetime.now(tz=datetime.timezone.utc)
-
-    if expire_timedelta:
-        expire = now + expire_timedelta
-    else:
-        expire = now + datetime.timedelta(minutes=expire_minutes)
-
-    to_encode.update(exp=expire, iat=now)
-
-    encoded = jwt.encode(
-        payload=to_encode,
-        key=private_key,
-        algorithm=algorithm,
-    )
-    return encoded
+log = setup_logging()
 
 
-def decode_jwt(
-    token: str | bytes,
-    public_key: str = settings.jwt.public_key_path.read_text(),
-    algorithm: str = settings.jwt.algorithm,
-):
-    """Decode JWT using public key and algorithm, verifying signature.
-
-    Parameters
-    ----------
-        token (str | bytes): JWT for decoding.
-        public_key (str): Public key for verifying the JWT signature.
-        algorithm (str): Algorithm for verifying JWT signature.
-
-    Returns
-    -------
-        Decoded data (payload) from the JWT.
-
-    """
-    decoded = jwt.decode(
-        jwt=token,
-        key=public_key,
-        algorithms=[algorithm],
-    )
-    return decoded
-
-
-# exc
 def hash_password(raw_password: str):
     """Hash raw password using Argon2 algorithm."""
-    return ph.hash(raw_password)
+    try:
+        return ph.hash(raw_password)
+    except HashingError:
+        raise PasswordHashingIsError from None
+    except Exception as exc:
+        raise PasswordHashError from exc
 
 
-# rehash and exc
 def check_password(raw_password: str, hash_password: str) -> bool:
     """Check whether enter password matches hashed password.
 
@@ -91,5 +30,12 @@ def check_password(raw_password: str, hash_password: str) -> bool:
     try:
         ph.verify(hash_password, raw_password)
         return True
-    except Exception:
+    except VerifyMismatchError:
+        log.debug("Пароль не совпадает c хешированным паролем")
+        return False
+    except InvalidHashError:
+        log.error("Хэш пустой или имеет неправильную структуру")
+        return False
+    except Exception as exc:
+        log.exception("Произошла неизвестная ошибка при проверке пароля: %s", exc)
         return False
