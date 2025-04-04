777e4cb12a453b16aa76c6de96caec0667f1558f
Alexander <sasha.beloglazoov@gmail.com>
2025-04-04 19:31:12 +0300

    add exception for create profile schema and delete unnessesary

--- api_v1/auth/exceptions.py
+++ api_v1/auth/exceptions.py
@@ -1,6 +1,26 @@
 from exceptions import BaseLogicError, BaseValidationError
 
 
+class ProfileValidationError(BaseValidationError):
+    """."""
+
+
+class DateOfBirthMinAgeError(ProfileValidationError):
+    """."""
+
+
+class DateOfBirthFutureError(ProfileValidationError):
+    """."""
+
+
+class ProfileNameOrSurnameInvalidCharacherError(ProfileValidationError):
+    """."""
+
+
+class ProfileNameOrSurnameNoWhitespaceError(ProfileValidationError):
+    """."""
+
+
 class UsernameOrEmailRequiredError(BaseValidationError):
     """Raised if username and email not specified during login."""
 
@@ -9,20 +29,12 @@ class UsernameValidationError(BaseValidationError):
     """Exception class for all username validation errors."""
 
 
-class UsernameTooShortOrTooLongError(UsernameValidationError):
-    """Raised when validate username. Its length less than 6 charachers."""
-
-
 class InvalidUsernameCharacherError(UsernameValidationError):
     """Raised when validate username. Its contain only allowed characters."""
 
 
 class PasswordValidationError(BaseValidationError):
     """Base exception class for all password validation errors."""
-
-
-class PasswordShortLengthError(PasswordValidationError):
-    """Raised when validate password. Its length less than 8 characters."""
 
 
 class PasswordNoDigitError(PasswordValidationError):
