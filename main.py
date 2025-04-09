e74adfa39ca82b3531a4eac71fc7edb63b7436d8
Alexander <sasha.beloglazoov@gmail.com>
2025-04-09 21:14:44 +0300

    add method lifespan for work app

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from core.models.db_helper import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    """After application shutdown, database engine shutdown."""
+    yield
+    await db_helper.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 if __name__ == "__main__":
