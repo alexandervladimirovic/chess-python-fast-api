79629f270c0d86ecbc9696a36f6a0fad3526d1b8
Alexander <sasha.beloglazoov@gmail.com>
2025-04-10 11:37:44 +0300

    add strict=True

--- schemas.py
+++ schemas.py
@@ -9,18 +9,20 @@ class ChessBaseSchema(BaseModel):
     types.
 
     Config:
+        - `arbitrary_types_allowed': Allows use arbitrary data types in models.
+        - `hide_input_in_errors': Hides input values in error messages.
+        - `strict`: Enables strict typing.
         - `str_strip_whitespace': Removes spaces at begin and end string.
         - `validate_assignment': Enables data validation when changing model fields.
-        - `arbitrary_types_allowed': Allows use arbitrary data types in models.
         - `validate_default': enables validation of default values during model
         initialization.
-        - `hide_input_in_errors': Hides input values in error messages.
     """
 
     model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        hide_input_in_errors=True,
+        strict=True,
         str_strip_whitespace=True,
         validate_assignment=True,
-        arbitrary_types_allowed=True,
         validate_default=True,
-        hide_input_in_errors=True,
     )
