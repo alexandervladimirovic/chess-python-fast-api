f2f818a436f5c94152243ca775ba330cfbb90283
Alexander <sasha.beloglazoov@gmail.com>
2025-04-11 14:27:45 +0300

    add constants

---
+++ api_v1/auth/constants.py
@@ -0,0 +1,34 @@
+# JWT
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+
+# Models
+# Max length for 'User' model
+MAX_LENGTH_USERNAME = 30
+MAX_LENGTH_EMAIL = 255
+MAX_LENGTH_PASSWORD_HASH = 255
+# Max length for 'Profile' model
+MAX_LENGTH_NAME = 30
+MAX_LENGTH_SURNAME = 40
+MAX_LENGTH_BIOGRAPHY = 300
+MAX_LENGTH_AVATAR_URL = 255
+# Max length for 'Country' model
+MAX_LENGTH_COUNTRY_NAME = 50
+MAX_LENGTH_COUNTRY_CODE = 2
+# Max length for 'Rank' model
+MAX_LENGTH_RANK_NAME = 30
+MAX_LENGTH_RANK_ABBREVIATION = 3
+# Max length for 'Role' model
+MAX_LENGTH_ROLE_NAME = 50
+# Max length for 'Privilege' model
+MAX_LENGTH_PRIVILEGE_NAME = 100
+
+# Schemas
+# User Schema
+MIN_LENGTH_USERNAME = 6
+MIN_LENGTH_PASSWORD = 8
+# Profile Schema
+MIN_LENGTH_NAME = 2
+MIN_LENGTH_SURNAME = 4
+MIN_AGE = 3
