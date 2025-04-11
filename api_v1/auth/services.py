6dc079ba5b932a2613584ac2eabe3fd08a3dd9ea
Alexander <sasha.beloglazoov@gmail.com>
2025-04-11 17:10:38 +0300

    add create user, get user by username

--- api_v1/auth/services.py
+++ api_v1/auth/services.py
@@ -1,1 +1,32 @@
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from .models import User
+from .schemas import UserRegisterSchema
+
+
+async def create_user(session: AsyncSession, user_in: UserRegisterSchema) -> User:
+    """Create new user in database."""
+    data = user_in.model_dump(exclude={"confirm_password"}, by_alias=True)
+
+    user = User(**data)
+
+    try:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
+    except IntegrityError:
+        await session.rollback()
+        raise
+
+
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    """Search user by username in database."""
+    stmt = select(User).where(User.username == username)
+
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    return user
